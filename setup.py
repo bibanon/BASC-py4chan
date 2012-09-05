fb4a71967def9e199504757fd90e244dd083a065
e <e@encyclopediadramatica.se>
2012-09-05 18:21:10 -0400

    Add requirements.txt, setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='py-4chan',
+    version='0.0.1',
+    description='Python 4chan API Wrapper',
+    author='Edgeworth Euler',
+    author_email='e@encyclopediadramatica.se/',
+    url='github.com/e000/py-4chan',
+    packages=['py4chan']
+)
