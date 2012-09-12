411b2077d276295ce07520b360e23cd175f95b34
e <e@encyclopediadramatica.se>
2012-09-12 11:53:08 -0400

    Update License.

--- README.md
+++ README.md
@@ -20,3 +20,22 @@ print "I fetched," thread.update(), "new replies."
 ```
 
 API Documentation coming soon, but you can figure most of it out from the source. It's not rocket science.
+
+# License
+
+``` text
+DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                    Version 2, December 2004
+
+ Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+
+ Everyone is permitted to copy and distribute verbatim or modified
+ copies of this license document, and changing it is allowed as long
+ as the name is changed.
+
+            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+  0. You just DO WHAT THE FUCK YOU WANT TO.
+
+```
--- setup.py
+++ setup.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
 
+"""
+    py-4chan-setup
+    ~~~~~~~~~~~~~~
+
+    Python Wrappers to access 4chan's API.
+
+    This program is free software. It comes without any warranty, to
+    the extent permitted by applicable law. You can redistribute it
+    and/or modify it under the terms of the Do What The Fuck You Want
+    To Public License, Version 2, as published by Sam Hocevar. See
+    http://sam.zoy.org/wtfpl/COPYING for more details.
+
+"""
+
 from distutils.core import setup
 from py4chan import _VERSION
 
@@ -9,5 +23,6 @@ setup(name='py-4chan',
     author='Edgeworth Euler',
     author_email='e@encyclopediadramatica.se',
     url='http://github.com/e000/py-4chan',
+    license="http://sam.zoy.org/wtfpl/COPYING",
     packages=['py4chan']
 )
