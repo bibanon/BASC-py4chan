5eec488c5e3e08f1cec9aa498c9cc7178c28cd72
antonizoon <sagnessagiel@gmail.com>
2014-03-16 12:21:28 -0700

    0.3.1 - Major bugfixes in md5sum and sticky and post

--- setup.py
+++ setup.py
@@ -15,12 +15,7 @@
 """
 
 from setuptools import setup
-#from py4chan import _VERSION		# Causes issues with requests-transition, when requests is not already installed. Use workaround
-_VERSION = '0.3'
-
-# Use requirements.txt file for dependencies
-# Disabled, because it doesn't work with PyPi
-#REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+from py4chan import _VERSION
 
 setup(name='py-4chan',
     version=_VERSION,
