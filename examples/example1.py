7d03b057bcd6028a6280bfa847b2a37d6a2d66ea
antonizoon <sagnessagiel@gmail.com>
2014-04-20 16:57:20 -0700

    examples now work on python 2 and 3

--- examples/example1.py
+++ examples/example1.py
@@ -5,23 +5,23 @@ import py4chan
 
 def main():
     v = py4chan.Board('v')
-    thread = v.get_thread(152900882)
-    print thread
-    print 'Sticky?', thread.sticky
-    print 'Closed?', thread.closed
+    thread = v.getThread(152900882)
+    print(thread)
+    print('Sticky?', thread.Sticky)
+    print('Closed?', thread.Closed)
     topic = thread.topic
-    print 'Topic Repr',   topic
-    print 'Post_number',  topic.post_number
-    print 'Timestamp',    topic.timestamp
-    print 'Date_time',    repr(topic.datetime)
-    print 'Image_md5_hex', topic.image_md5_hex
-    print 'Image_url',     topic.image_url
-    print 'Subject',      topic.subject
-    print 'Comment',      topic.comment
-    print 'Thumbnail_url',topic.thumbnail_url
-    print 'Replies',      thread.replies
-    print 'All_posts',    thread.all_posts
-    print 'Thread_url',    thread.thread_url
+    print('Topic Repr', topic)
+    print('PostNumber', topic.PostNumber)
+    print('Timestamp',  topic.Timestamp)
+    print('DateTime',   repr(topic.Datetime))
+    print('FileMd5Hex', topic.FileMd5Hex)
+    print('FileUrl',    topic.FileUrl)
+    print('Subject',    topic.Subject)
+    print('Comment',    topic.Comment)
+    print('ThumbnailUrl',topic.ThumbnailUrl)
+    print('Replies',    thread.replies)
+    print('AllPosts',    thread.AllPosts)
+    print('ThreadUrl',  thread.ThreadUrl)
 
 if __name__ == '__main__':
     main()
