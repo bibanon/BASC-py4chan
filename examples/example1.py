21896e918c49c00ffcbcbdc3054ea9cde77d10ed
Antonizoon <antonizoon@bibanon.org>
2014-11-16 21:05:52 -0500

    examples now cross compatible with both python 2.x/3.x

--- examples/example1.py
+++ examples/example1.py
@@ -8,21 +8,21 @@ import py4chan
 def main():
     v = py4chan.Board('v')
     thread = v.get_thread(152900882)
-    print thread
-    print 'Sticky?', thread.sticky
-    print 'Closed?', thread.closed
+    print(thread)
+    print('Sticky?', thread.sticky)
+    print('Closed?', thread.closed)
     topic = thread.topic
-    print 'Topic Repr', topic
-    print 'Postnumber', topic.post_number
-    print 'Timestamp',  topic.timestamp
-    print 'Datetime',   repr(topic.datetime)
-    print 'Filemd5hex', topic.file_md5_hex
-    print 'Fileurl',    topic.file_url
-    print 'Subject',    topic.subject
-    print 'Comment',    topic.comment
-    print 'Thumbnailurl',topic.thumbnail_url
-    print 'Replies',    thread.replies
-    print 'All posts',  thread.all_posts
+    print('Topic Repr', topic)
+    print('Postnumber', topic.post_number)
+    print('Timestamp',  topic.timestamp)
+    print('Datetime',   repr(topic.datetime))
+    print('Filemd5hex', topic.file_md5_hex)
+    print('Fileurl',    topic.file_url)
+    print('Subject',    topic.subject)
+    print('Comment',    topic.comment)
+    print('Thumbnailurl',topic.thumbnail_url)
+    print('Replies',    thread.replies)
+    print('All posts',  thread.all_posts)
 
 
 if __name__ == '__main__':
--- examples/example3.py
+++ examples/example3.py
@@ -8,12 +8,12 @@ import py4chan
 
 b = py4chan.Board('b')
 threads = b.get_threads()
-print "Got %i threads" % len(threads)
+print("Got %i threads" % len(threads))
 first_thread = threads[0]
-print "First thread: %r" % first_thread
-print "Replies: %r" % first_thread.replies
-print "Updating first thread..."
+print("First thread: %r" % first_thread)
+print("Replies: %r" % first_thread.replies)
+print("Updating first thread...")
 first_thread.update()
-print "First thread now: %r" % first_thread
+print("First thread now: %r" % first_thread)
 for post in first_thread.replies:
-    print post.post_url
+    print(post.post_url)
