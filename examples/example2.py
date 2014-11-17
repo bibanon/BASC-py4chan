a4478ba38cbca4abba03c252e604835d19fe1e5d
antonizoon <sagnessagiel@gmail.com>
2014-11-16 21:05:52 -0500

    examples now cross compatible with both python 2.x/3.x

--- examples/example2.py
+++ examples/example2.py
@@ -8,15 +8,15 @@ import py4chan
 
 def main():
     if len(sys.argv) != 3:
-        print "Usage: python %s [board] [thread]" % sys.argv[0]
-        print "Shows the URL of all the files in the thread."
-        print "Example (download all files in thread): python %s v 12351234 | xargs wget" % sys.argv[0]
+        print("Usage: python %s [board] [thread]" % sys.argv[0])
+        print("Shows the URL of all the files in the thread.")
+        print("Example (download all files in thread): python %s v 12351234 | xargs wget" % sys.argv[0])
         return
 
     board = py4chan.Board(sys.argv[1])
     thread = board.get_thread(int(sys.argv[2]))
     for file in thread.files():
-        print file
+        print(file)
 
 if __name__ == '__main__':
     main()
