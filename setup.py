cb20301941054387f76e4a94fb962004605b5c0c
Daniel Oaks <daniel@danieloaks.net>
2014-12-09 20:29:47 +1000

    Docstring fun! Making readme nicer!

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""4chan Python Library.
 
-"""
-    py-4chan-setup
-    ~~~~~~~~~~~~~~
+BASC-py4chan is a Python library that gives access to the 4chan API
+and an object-oriented way to browse and get board and thread
+information quickly and easily.
 
-    Python Wrappers to access 4chan's API.
-
-    This program is free software. It comes without any warranty, to
-    the extent permitted by applicable law. You can redistribute it
-    and/or modify it under the terms of the Do What The Fuck You Want
-    To Public License, Version 2, as published by Sam Hocevar. See
-    http://sam.zoy.org/wtfpl/COPYING for more details.
-
+This program is free software. It comes without any warranty, to
+the extent permitted by applicable law. You can redistribute it
+and/or modify it under the terms of the Do What The Fuck You Want
+To Public License, Version 2, as published by Sam Hocevar. See
+the LICENSE file for more details.
 """
 
 from setuptools import setup
