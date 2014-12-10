34d8fdec2b2e5a4efae4bd823ba56a6cddbfa209
Daniel Oaks <daniel@danieloaks.net>
2014-12-10 10:53:35 +1000

    Split module documentation into its own file, fix docs

--- basc_py4chan/__init__.py
+++ basc_py4chan/__init__.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# This program is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What The Fuck You Want
+# To Public License, Version 2, as published by Sam Hocevar. See
+# the LICENSE file for more details.
 """4chan Python Library.
 
 BASC-py4chan is a Python library that gives access to the 4chan API
 and an object-oriented way to browse and get board and thread
 information quickly and easily.
-
-This program is free software. It comes without any warranty, to
-the extent permitted by applicable law. You can redistribute it
-and/or modify it under the terms of the Do What The Fuck You Want
-To Public License, Version 2, as published by Sam Hocevar. See
-the LICENSE file for more details.
 """
 
 __version__ = '0.4.2'
---
+++ docs/library/py4chan.rst
@@ -0,0 +1,17 @@
+:mod:`basc_py4chan` – 4chan Python Library
+==========================================
+
+:class:`basc_py4chan.Board` provides access to a 4chan board including checking if threads exist, retrieving appropriate :class:`basc_py4chan.Thread` objects, and returning lists of all the threads that exist on the given board.
+
+
+Basic Usage
+-----------
+
+.. automodule:: basc_py4chan
+
+Methods
+-------
+
+    .. autofunction:: basc_py4chan.get_boards
+
+    .. autofunction:: basc_py4chan.get_all_boards
