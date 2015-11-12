e4740ffa789aa8a819c82b0639f7d98b68c84d4e
Antonizoon <antonizoon@bibanon.org>
2015-11-12 16:38:09 -0500

    should really test examples before pushing at all

--- examples/example5-allfiles.py
+++ examples/example5-allfiles.py
@@ -4,13 +4,13 @@ import basc_py4chan
 
 def main():
     # grab the first thread on the board by checking first page
-    board = py8chan.Board('v')
+    board = basc_py4chan.Board('v')
     all_thread_ids = board.get_all_thread_ids()
     first_thread_id = all_thread_ids[0]
     thread = board.get_thread(first_thread_id)
 
     # display the url of every file on the first thread, even extra files in posts
-    for url in first_thread.files():
+    for url in thread.files():
         print(url)
 
 if __name__ == '__main__':
