7abe762a3c5fcab8bc376b111720118136a2e8df
antonizoon <sagnessagiel@gmail.com>
2015-11-12 16:34:45 -0500

    typo with example, should probably test them before pushing

--- examples/example4-file-object.py
+++ examples/example4-file-object.py
@@ -4,21 +4,20 @@ import basc_py4chan
 
 def main():
     # grab the first thread on the board by checking first page
-    v = basc_py4chan.Board('v')
+    board = basc_py4chan.Board('v')
     all_thread_ids = board.get_all_thread_ids()
     first_thread_id = all_thread_ids[0]
     thread = board.get_thread(first_thread_id)
 
     # display info about every file on the first thread, even extra files in posts
-    for post in first_thread.all_posts:
+    for post in thread.all_posts:
         if post.has_file:
             print(":: Post #", post.post_number)
-            for file in post.all_files():
-                print("  ", file.filename)
-                print("  ", file.file_md5_hex)
-                print("  ", file.file_url)
-                print("  ", file.thumbnail_url)
-                print()
+            print("  ", post.file.filename)
+            print("  ", post.file.file_md5_hex)
+            print("  ", post.file.file_url)
+            print("  ", post.file.thumbnail_url)
+            print()
 
 if __name__ == '__main__':
     main()
