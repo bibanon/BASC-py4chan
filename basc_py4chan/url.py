3b8e89aa04bf8062c7114e4cd6ef5c88dd95d740
Antonizoon <antonizoon@bibanon.org>
2017-02-06 22:46:25 -0500

    0.6.3 - Just revert back to i.4cdn.org since 4chanX uses it too0.6.3 - Just revert back to i.4cdn.org since 4chanX uses it too

--- basc_py4chan/url.py
+++ basc_py4chan/url.py
@@ -12,8 +12,9 @@ class Url(object):
         DOMAIN = {
             'api': self._protocol + 'a.4cdn.org',   # API subdomain
             'boards': self._protocol + 'boards.4chan.org', # HTML subdomain
-            'file': self._protocol + 'is.4chan.org',  # file (image) host
-            'thumbs': self._protocol + 'is.4chan.org',# thumbs host
+            'file': self._protocol + 'i.4cdn.org',  # file (image) host
+            #'file': self._protocol + 'is.4chan.org', # new, slower image host
+            'thumbs': self._protocol + 'i.4cdn.org',# thumbs host
             'static': self._protocol + 's.4cdn.org' # static host
         }
         
